4a6166231ca10f7574fef0b60611d7b747060155
Swiiish21 <malcolmotieno@gmail.com>
2024-07-04 11:03:48 +0300

    javascript basics

---
+++ operators.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 15;
+let c = 5;
+
+let sum = a + b;
+let diff = b - c;
+let div = b / c;
+let multiplication = a * c;
+
+console.log(sum)
+console.log(diff)
+console.log(div)
+console.log("The"+ multiplication +" result of the product of a and c is: " + multiplication)
---
+++ script.js
@@ -0,0 +1,7 @@
+let name = "John";
+let age = 25;
+let is_student = true;
+
+console.log(name)
+console.log(age)
+console.log(is_student)
