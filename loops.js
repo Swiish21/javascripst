2657bdf6bd9cdddadafc123a4d008d016a89c5a2
Swiiish21 <malcolmotieno@gmail.com>
2024-07-15 09:52:23 +0300

    loooooooppppsssss

---
+++ loops.js
@@ -0,0 +1,18 @@
+//while loop that will print 0, 1, 2, 3, 4
+let i = 0
+while(i < 5) {
+    console.log('While loop iterration ' + i);
+    i++;
+}
+
+//for loop that will print 0, 1, 2, 3, 4
+for(let i = 0; i < 5; i++) {
+    console.log('For loop iterration ' + i);
+}
+
+//do while loop that will print 0, 1, 2, 3, 4
+let i2 = 0
+do {
+    console.log('Do while loop iterration ' + i2);
+    i2++;
+} while(i2 < 5)
