b93cb4a7ffa1c4e07af9b06d6773a15dba56f126
Malcom <malcolmotieno16@gmail.com>
2024-07-16 11:37:12 +0300

    arrays2

---
+++ arrays2.js
@@ -0,0 +1,15 @@
+// let fruits = ['apple', 'banana', 'orange'];
+
+// fruits[3] = 'mango', 'pineapple', 'guava';
+
+// for(let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+let nums = [1, 4, 7, 2, 0, 8];
+let sum = 0
+
+for(let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+}
+console.log(sum)
