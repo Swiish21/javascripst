aa544d12b5984f4043e010085e38e773a9a1330a
Swiiish21 <malcolmotieno@gmail.com>
2024-09-04 13:38:24 +0300

    ...

--- vfnjk.js
+++ vfnjk.js
@@ -1,19 +1,20 @@
 // console.log(process.argv);
+
 // const arguments = process.argv.slice(2);
 // const sum = arguments.reduce((acc, val) => acc + parseInt(val), 0);
 // console.log(sum);
 
-// const arguments = process.argv.slice(2);
+const arguments = process.argv.slice(2);
 // const myVar = arguments.reduce((acc, val) => acc + '' + val, '');
-// console.log(myVar);
+console.log(arguments);
 
 // const js1 = `"JavaScript is awesome"`;
 // console.log(js1);
 
 
-const statement1 = "C is fun";
-const statement2 = "Python is cool";
-const statement3 = "JavaScript is amazing";
-console.log(statement1);
-console.log(statement2);
-console.log(statement3);
+// const statement1 = "C is fun";
+// const statement2 = "Python is cool";
+// const statement3 = "JavaScript is amazing";
+// console.log(statement1);
+// console.log(statement2);
+// console.log(statement3);
