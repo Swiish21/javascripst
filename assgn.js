372451f19bc2161a074cb4eee75813dcabd86be5
Swiiish21 <malcolmotieno@gmail.com>
2024-09-04 13:19:19 +0300

    ...

---
+++ assgn.js
@@ -0,0 +1,11 @@
+function print_num() {
+    let i = 0
+    // let result = 0
+    while(i <= 15) {
+      console.log(i);
+      i++;
+    }
+    return "Done!";
+  }
+let result = print_num();
+console.log(result);
