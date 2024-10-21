bd1cae05e2552a52b69e0e7511de5b6149870152
Swiiish21 <malcolmotieno@gmail.com>
2024-10-21 10:42:20 +0300

    zidooo

---
+++ training.js
@@ -0,0 +1,26 @@
+//loop1
+// prints all numbers from 0 to 20
+for(i = 0; i <= 20; i++) {
+    console.log(i);
+} 
+
+//loop2
+// prints all odd numbers from 3 to 29
+for(let a = 3; a <= 29; a+= 2) {
+    console.log(a);
+}
+
+//loop3
+// prints all numbers from 12 to -14
+for(let b = 12; b >= -14; b--) {
+    console.log(b);
+}
+
+//loop4
+// prints all numbers from 20 to 50 that are divisible by 3
+for (c = 50; c >= 20; c--) {
+    if(c % 3 === 0) {
+        console.log(c);
+    }
+}
+
